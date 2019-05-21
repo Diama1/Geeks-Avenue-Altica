c698ab28a216d115c237af1c3cc7c9a302025ee7
Frank Mutabazi <frankmutabazi@Franks-MacBook-Pro.local>
2019-05-21 20:23:58 +0200

    Environment Setup

---
+++ config/config.js
@@ -0,0 +1,27 @@
+const dotenv=require("dotenv");
+
+dotenv.config();
+
+module.exports={
+    development: {
+        "username": process.env.USERNAME,
+        "password": process.env.PASSWORD,
+        "database": process.env.DATABASE,
+        "host": process.env.HOST,
+        "dialect": process.env.DIALECT
+      },
+      test: {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+      },
+      production: {
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+      }
+}
