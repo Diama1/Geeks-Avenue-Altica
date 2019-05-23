377cf9aaf536530166492f7f7a7d66541034cdc8
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-05-23 09:32:02 +0200

    Add more validations about articles when a user is creating one

--- helpers/Validations.helper.js
+++ helpers/Validations.helper.js
@@ -5,7 +5,7 @@ class Validations {
         const schema = Joi.object().keys({
             fullNames: Joi.string().min(5).required(),
             email: Joi.string().email().required(),
-            password: Joi.string.regex(/[a-zA-Z0-9]{6,15}/).required(),
+            password: Joi.string().regex(/[a-zA-Z0-9]{6,15}/).required(),
         });
 
         return Joi.validate(user, schema);
@@ -17,6 +17,15 @@ class Validations {
             password: Joi.string().regex(/[a-zA-Z0-9]{6,15}/).required(),
         });
         return Joi.validate(user, schema);
+    }
+
+    static articleValidate(article) {
+        const schema = Joi.object().keys({
+            title: Joi.string().required(),
+            description: Joi.string().required(),
+            category: Joi.string().required(),
+        });
+        return Joi.validate(article, schema);
     }
 }
 
--- middlewares/Auth.middleware.js
+++ middlewares/Auth.middleware.js
@@ -1,31 +1,30 @@
 import jwt from "jsonwebtoken";
 
-class Auth{
-    static verifyToken(req,res,next){
-        const bearerheader=req.headers.authorization;
-        if(typeof bearerheader!=="undefined"){
-            const bearer=bearerheader.split(" ");
-            const bearerToken=bearer[1];
-            req.token=bearerToken;
+class Auth {
+    static verifyToken(req, res, next) {
+        const bearerheader = req.headers.authorization;
+        if (typeof bearerheader !== "undefined") {
+            const bearer = bearerheader.split(" ");
+            const bearerToken = bearer[1];
+            req.token = bearerToken;
 
 
-            jwt.verify(req.token,process.env.SECRET_KEY,(err,userData)=>{
-                if(err){
+            jwt.verify(req.token, process.env.SECRET_KEY, (err, userData) => {
+                if (err) {
                     res.status(401).json({
-                        status:401,
-                        error:"You provided an invalid token"
-                    })
-                }
-                else{
+                        status: 401,
+                        error: "You provided an invalid token",
+                    });
+                } else {
+                    req.user = userData;
                     next();
                 }
-            })
-        }
-        else{
+            });
+        } else {
             res.status(401).json({
-                status:401,
-                error:"You are not authenticated!"
-            })
+                status: 401,
+                error: "You are not authenticated!",
+            });
         }
     }
 }
