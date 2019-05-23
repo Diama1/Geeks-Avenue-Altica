6636d17f4c02f2e450bf4d8b8cc9df040d6a1c1b
Diama1 <32199791+Diama1@users.noreply.github.com>
2019-05-23 13:05:42 +0200

    this commit is for fetching all the articles from the database

--- routers/Article.route.js
+++ routers/Article.route.js
@@ -5,6 +5,7 @@ import ArticleController from "../controllers/Article.controller";
 const router = Router();
 
 router.post("/", Auth.verifyToken, ArticleController.createStory);
+router.get("/", Auth.verifyToken, ArticleController.getAllArticles);
 router.delete("/:articleId", Auth.verifyToken, ArticleController.deleteStory);
 
 export default router;
