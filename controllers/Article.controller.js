145a0897705b90f644abcaf0ee96bac7e7abcab0
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-05-23 12:13:51 +0200

    User should be able to delete his own story

--- controllers/Article.controller.js
+++ controllers/Article.controller.js
@@ -35,6 +35,40 @@ class ArticleController {
             });
         }
     }
+
+    /**
+     * @static
+     *
+     * @param {*} req - request
+     * @param {*} res -response
+     */
+    static async deleteStory(req, res) {
+        const { id } = req.user;
+        const { articleId } = req.params;
+        const user = await User.findOne({ where: { id } });
+        if (Object.keys(user.dataValues).length) {
+            const article = await Article.findOne({ where: { id: articleId } });
+            if (Object.keys(article.dataValues).length) {
+                const result = await Article.destroy({ where: { id: articleId }, returning: true });
+                if (result) {
+                    res.json({
+                        status: 200,
+                        message: "Deleted!",
+                    });
+                } else {
+                    res.status(500).json({
+                        status: 500,
+                        error: "Sorry,this article can't be deleted",
+                    });
+                }
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    error: "Article is not available to be deleted!",
+                });
+            }
+        }
+    }
 }
 
 export default ArticleController;
