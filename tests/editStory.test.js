c02d0e328fcbda0891b4fbb3ca78c5b9528a0b06
Frank Mutabazi <frankmutabazi@Franks-MacBook-Pro-2.local>
2019-05-31 14:42:54 +0200

    removing unnecessary tests

--- tests/editStory.test.js
+++ tests/editStory.test.js
@@ -12,7 +12,7 @@ chai.use(chaiHttp);
 
 // @Bring In our dummy Data for the purpose of making the Tests
 const { user, user1 } = dummyData.users;
-const { validArticle, invalidArticle } = dummyData.updateArticle;
+const { validArticle, invalidArticle, validComment } = dummyData.updateArticle;
 const { Article } = db;
 
 // @ Generate a token to be used - invalid token and the valid token
@@ -30,8 +30,7 @@ describe("PUT /api/articles/:articleId", () => {
     });
 
     it("should return an error message for the invalid articleId provided", () => {
-        chai
-            .request(app)
+        chai.request(app)
             .patch("/api/v1/articles/1fadfa")
             .set("Authorization", `Bearer ${token}`)
             .send(validArticle)
@@ -43,12 +42,12 @@ describe("PUT /api/articles/:articleId", () => {
     });
 
     it("should return an error message if the article is not found", () => {
-        chai
-            .request(app)
+        chai.request(app)
             .patch("/api/v1/articles/10000")
             .set("Authorization", `Bearer ${token}`)
             .send(validArticle)
             .end((err, res) => {
+                console.log(res.body);
                 expect(res.body).to.be.an("object");
                 expect(res.body.status).to.deep.equal(404);
                 expect(res.body.error).to.deep.equal("This Article is not found!");
@@ -56,8 +55,7 @@ describe("PUT /api/articles/:articleId", () => {
     });
 
     it("should return an error message that you are not the owner of the story", () => {
-        chai
-            .request(app)
+        chai.request(app)
             .patch("/api/v1/articles/1")
             .set("Authorization", `Bearer ${invalidToken}`)
             .send(validArticle)
@@ -69,8 +67,7 @@ describe("PUT /api/articles/:articleId", () => {
     });
 
     it("should validate the title,description and category if are string", (done) => {
-        chai
-            .request(app)
+        chai.request(app)
             .patch("/api/v1/articles/1")
             .set("Authorization", `Bearer ${token}`)
             .send(invalidArticle)
@@ -83,8 +80,7 @@ describe("PUT /api/articles/:articleId", () => {
     });
 
     it("should update successfully the article", () => {
-        chai
-            .request(app)
+        chai.request(app)
             .patch("/api/v1/articles/1")
             .set("Authorization", `Bearer ${token}`)
             .send(validArticle)
@@ -92,95 +88,6 @@ describe("PUT /api/articles/:articleId", () => {
                 expect(res.body).to.be.an("object");
                 expect(res.body.status).to.deep.equal(200);
                 expect(res.body.message).to.deep.equal("Article successfully updated...");
-            });
-    });
-});
-
-describe("Delete /api/v1/articles/:articleId/comments/:commentId", () => {
-    beforeEach("create a new comment for a specific article", (done) => {
-        chai
-            .request(app)
-            .post("/api/v1/articles/3/comments")
-            .set("Authorization", `Bearer ${token}`)
-            .send(validComment)
-            .end((err, res) => {
-                console.log("====delete a comment story=====");
-                console.log(res.body);
-                // expect(res.body).to.be.an('object');
-                done();
-            });
-    });
-
-    it("should return an error message for the invalid articleId or commentId provided", () => {
-        chai
-            .request(app)
-            .delete("/api/v1/articles/1fadfa/comments/1dafdasf")
-            .set("Authorization", `Bearer ${token}`)
-            .end((err, res) => {
-                expect(res.body).to.be.an("object");
-                expect(res.body.status).to.deep.equal(400);
-                expect(res.body.error).to.deep.equal("Invalid Article ID");
-            });
-    });
-
-    it("should return an error message if the article is not found", () => {
-        chai
-            .request(app)
-            .delete("/api/v1/articles/10000/comments/1")
-            .set("Authorization", `Bearer ${token}`)
-            .end((err, res) => {
-                expect(res.body).to.be.an("object");
-                expect(res.body.status).to.deep.equal(404);
-                expect(res.body.error).to.deep.equal("This Article is not found!");
-            });
-    });
-
-    it("should return an error message if the comment is not found", () => {
-        chai
-            .request(app)
-            .delete("/api/v1/articles/3/comments/10000")
-            .set("Authorization", `Bearer ${token}`)
-            .end((err, res) => {
-                expect(res.body).to.be.an("object");
-                expect(res.body.status).to.deep.equal(404);
-                expect(res.body.error).to.deep.equal("This Comment is not found!");
-            });
-    });
-
-    it("should return an error message that you are not the owner of the comment", () => {
-        chai
-            .request(app)
-            .delete("/api/v1/articles/3/comments/1")
-            .set("Authorization", `Bearer ${invalidToken}`)
-            .end((err, res) => {
-                expect(res.body).to.be.an("object");
-                expect(res.body.status).to.deep.equal(400);
-                expect(res.body.error).to.deep.equal("Sorry! You are not the Owner of This comment");
-            });
-    });
-
-    it("should delete a comment successfully", () => {
-        chai
-            .request(app)
-            .delete("/api/v1/articles/3/comments/1")
-            .set("Authorization", `Bearer ${token}`)
-            .end((err, res) => {
-                console.log("====");
-                console.log(res.body);
-                expect(res.body).to.be.an("object");
-                expect(res.body.status).to.deep.equal(200);
-                expect(res.body.message).to.deep.equal("Comment deleted successfully!");
-            });
-    });
-
-    it("Should be able to delete an article he owns", () => {
-        chai.request(app)
-            .delete("/api/v1/articles/3")
-            .set("Authorization", `Bearer ${token}`)
-            .end((err, res) => {
-                console.log("====");
-                console.log(res.body);
-                expect(res.body).to.be.an("object");
             });
     });
 });
