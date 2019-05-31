567473143d36e57619d8c95cbc2ef22e0f996ba3
Frank Mutabazi <frankmutabazi@Franks-MacBook-Pro-2.local>
2019-05-31 09:33:18 +0200

    linting the files

--- tests/mockupData/dummyData.js
+++ tests/mockupData/dummyData.js
@@ -1,24 +1,24 @@
-export default{
-  users:{
-    user:{
-      id:1,
-      email:"u.audace@test.com"
+export default {
+    users: {
+        user: {
+            id: 1,
+            email: "u.audace@test.com",
+        },
+        user1: {
+            id: 4,
+            email: "u.audace@test.com",
+        },
     },
-    user1:{
-      id:4,
-      email:"u.audace@test.com"
-    }
-  },
-  updateArticle:{
-    validArticle:{
-      title:"this is the article one",
-      description:"this is the description that has been created by uhiriwe audace",
-      category:"technology"
+    updateArticle: {
+        validArticle: {
+            title: "this is the article one",
+            description: "this is the description that has been created by uhiriwe audace",
+            category: "technology",
+        },
+        invalidArticle: {
+            title: 54554,
+            description: "this is the description that has been created by uhiriwe audace",
+            category: 4545,
+        },
     },
-    invalidArticle:{
-      title:54554,
-      description:"this is the description that has been created by uhiriwe audace",
-      category:4545
-    }
-  }
-}
+};
