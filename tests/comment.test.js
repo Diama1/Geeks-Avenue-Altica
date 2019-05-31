8951de16268a3256f228f78343963d5e46d1e726
Frank Mutabazi <frankmutabazi@Franks-MacBook-Pro-2.local>
2019-05-31 14:28:20 +0200

    adding tests

--- tests/comment.test.js
+++ tests/comment.test.js
@@ -93,6 +93,48 @@ describe("comment on an article test", () => {
             });
     });
 
+    it("should be able to update a comment and not the owner", (done) => {
+        chai.request(app)
+            .patch("/api/v1/articles/1/comments/1")
+            .set("Authorization", `Bearer ${invalidToken}`)
+            .send({
+                description: "yeah you deserve better",
+            })
+            .end((err, res) => {
+                expect(res.body).to.have.property("status").eql(403);
+                expect(res.body).to.have.property("error").eql("You are not allowed to modify this comment!");
+                done();
+            });
+    });
+
+    it("should be able to update a comment and not find the comment", (done) => {
+        chai.request(app)
+            .patch("/api/v1/articles/1/comments/2")
+            .set("Authorization", `Bearer ${invalidToken}`)
+            .send({
+                description: "yeah you deserve better",
+            })
+            .end((err, res) => {
+                expect(res.body).to.have.property("status").eql(404);
+                expect(res.body).to.have.property("error").eql("That comment is not available");
+                done();
+            });
+    });
+
+    it("should be able to update a comment and not find the article", (done) => {
+        chai.request(app)
+            .patch("/api/v1/articles/2/comments/2")
+            .set("Authorization", `Bearer ${invalidToken}`)
+            .send({
+                description: "yeah you deserve better",
+            })
+            .end((err, res) => {
+                expect(res.body).to.have.property("status").eql(404);
+                expect(res.body).to.have.property("error").eql("The article is not available!");
+                done();
+            });
+    });
+
     it("should be able to get his single comment", (done) => {
         chai.request(app)
             .get("/api/v1/articles/1/comments/1")
